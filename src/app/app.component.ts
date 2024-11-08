90784bc975f9bc99924bb80c1159851d2b68c893
Marcelina Parchatko <marcelina.parchatko@comarch.pl>
2024-11-08 15:12:57 +0100

    Projekt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MessagesComponent } from './messages/messages.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MessagesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'new-project';
+  title = 'Tour of Heroes';
 }
